6682b80a3efabccb56caa9b60fff60a72662465e
Aleksey Sidorov <sauron1987@gmail.com>
2022-11-28 00:09:21 +0400

    Test propagatedBuildInputs

--- examples/hello_world/build-images.sh
+++ examples/hello_world/build-images.sh
@@ -7,11 +7,3 @@ docker load < $(nix-build -A dockerImage --argstr config "x86_64-unknown-linux-m
 echo "-> compiling for gnu64 target"
 nix-shell -A shell --argstr config "x86_64-unknown-linux-gnu"  --run "cargo build --release"
 docker load < $(nix-build -A dockerImage --argstr config "x86_64-unknown-linux-gnu")
-
-echo "-> compiling for gnu-static target"
-nix-shell -A shell --argstr config "x86_64-unknown-linux-gnu" --arg isStatic true  --run "cargo build --release"
-docker load < $(nix-build -A dockerImage --argstr config "x86_64-unknown-linux-gnu" --arg isStatic true)
-
-echo "-> compiling for musl64-static target"
-nix-shell -A shell --argstr config "x86_64-unknown-linux-musl" --arg isStatic true  --run "cargo build --release"
-docker load < $(nix-build -A dockerImage --argstr config "x86_64-unknown-linux-musl" --arg isStatic true)
--- examples/hello_world/default.nix
+++ examples/hello_world/default.nix
@@ -21,20 +21,20 @@ let
     src = lockFile.nixpkgs;
   };
 in
-{
+rec {
   shell = pkgs.stdenv.mkDerivation {
     name = "shell-cross";
     strictDeps = true;
     nativeBuildInputs = [ pkgs.rustCrossHook ];
+    propagatedBuildInputs = [ pkgs.openssl.dev ];
   };
-
-  inherit pkgs;
 
   dockerImage = pkgs.dockerTools.buildLayeredImage {
     name = "hello_world";
     tag = crossSystem.config + (if isStatic then "-static" else "");
 
     contents = [
+      shell.propagatedBuildInputs
       (pkgs.copyBinaryFromCargoBuild {
         name = "hello_world";
         targetDir = ./target;
