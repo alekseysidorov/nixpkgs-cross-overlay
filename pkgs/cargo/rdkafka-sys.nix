e9d2a9b250a9cb81e64cec5aba20c89600047116
Aleksey Sidorov <sauron1987@gmail.com>
2022-12-20 15:07:18 +0400

    Polish rust cross hook

--- pkgs/cargo/rdkafka-sys.nix
+++ pkgs/cargo/rdkafka-sys.nix
@@ -24,12 +24,16 @@ let
         zlib.dev
         lz4
         zstd
+        rdkafka
       ];
       # We can force a several cargo features in the rdkafka
       envVariables = {
+        CARGO_FEATURE_DYNAMIC_LINKING = true;
         CARGO_FEATURE_EXTERNAL_LZ4 = true;
         CARGO_FEATURE_ZSTD_PKG_CONFIG = true;
         CARGO_FEATURE_SSL = true;
+        OPENSSL_INCLUDE_DIR = "${openssl.dev}/include";
+        OPENSSL_ROOT_DIR = "${openssl.out}";
       };
     } else {
       # We can just rdkafka nix package.
@@ -50,6 +54,16 @@ in
 mkEnvHook {
   name = "cargo-rdkafka-sys";
 
-  deps = out.deps;
-  envVariables = out.envVariables;
+  deps = [
+    pkg-config
+    rdkafka
+    openssl.dev
+  ];
+
+  envVariables = {
+    CARGO_FEATURE_DYNAMIC_LINKING = true;
+    CARGO_FEATURE_EXTERNAL_LZ4 = true;
+    CARGO_FEATURE_ZSTD_PKG_CONFIG = true;
+    CARGO_FEATURE_SSL = true;
+  };
 }
--- tests/build_cargo_deps.sh
+++ tests/build_cargo_deps.sh
@@ -4,5 +4,6 @@ set -euo
 
 script_dir="$( dirname -- "$BASH_SOURCE"; )";
 
+cargo clean --manifest-path ${script_dir}/Cargo.toml
 cargo build -vv --manifest-path ${script_dir}/Cargo.toml
 cargo clean --manifest-path ${script_dir}/Cargo.toml
