e8febb29acca23e2c9f511f0c15a0652a78bf198
Aleksey Sidorov <sauron1987@gmail.com>
2022-12-21 16:25:55 +0400

    Fix static builds

--- pkgs/cargo/rdkafka-sys.nix
+++ pkgs/cargo/rdkafka-sys.nix
@@ -9,22 +9,49 @@
 , pkgs
 }:
 
+let
+  isStatic = stdenv.targetPlatform.isStatic;
+  pkg-config = pkgs.pkgsBuildHost.pkg-config;
+  cmake = pkgs.pkgsBuildHost.cmake;
+
+  out =
+    if isStatic then {
+      # Since there is lack of static linking via pkg-config in rdkafka-sys we
+      # cannot use the rdkafka nix package.
+      deps = [
+        lz4
+        openssl.dev
+        rdkafka
+        zlib.dev
+        zstd
+      ];
+      # We can force a several cargo features in the rdkafka
+      envVariables = {
+        CARGO_FEATURE_EXTERNAL_LZ4 = true;
+        CARGO_FEATURE_ZSTD_PKG_CONFIG = true;
+        CARGO_FEATURE_SSL_VENDORED = true;
+      };
+    } else {
+      # We can just rdkafka nix package.
+      deps = [
+        lz4
+        openssl.dev
+        rdkafka
+        zlib.dev
+        zstd
+      ];
+
+      envVariables = {
+        CARGO_FEATURE_DYNAMIC_LINKING = true;
+        CARGO_FEATURE_EXTERNAL_LZ4 = true;
+        CARGO_FEATURE_ZSTD_PKG_CONFIG = true;
+        CARGO_FEATURE_SSL = true;
+      };
+    };
+in
 mkEnvHook {
   name = "cargo-rdkafka-sys";
 
-  deps = [
-    lz4
-    openssl.dev
-    openssl.dev
-    rdkafka
-    zlib.dev
-    zstd
-  ];
-
-  envVariables = {
-    CARGO_FEATURE_DYNAMIC_LINKING = true;
-    CARGO_FEATURE_EXTERNAL_LZ4 = true;
-    CARGO_FEATURE_ZSTD_PKG_CONFIG = true;
-    CARGO_FEATURE_SSL = true;
-  };
+  deps = out.deps;
+  envVariables = out.envVariables;
 }
--- tests/build_cargo_deps.sh
+++ tests/build_cargo_deps.sh
@@ -6,4 +6,3 @@ script_dir="$( dirname -- "$BASH_SOURCE"; )";
 
 cargo clean --manifest-path ${script_dir}/Cargo.toml
 cargo build -vv --manifest-path ${script_dir}/Cargo.toml
-cargo clean --manifest-path ${script_dir}/Cargo.toml
