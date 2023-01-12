4d1673f8d1bcf1e4bcafad33470b5a5a36f4c1b2
Aleksey Sidorov <sauron1987@gmail.com>
2023-01-12 14:42:50 +0400

    Fix rustflags completely

--- pkgs/hooks/rust-cross-hook.sh
+++ pkgs/hooks/rust-cross-hook.sh
@@ -3,8 +3,7 @@ populateRustCrossEnv () {
     # Setup cargo
     export CARGO_BUILD_TARGET=@cargoBuildTarget@
     export CARGO_TARGET_@cargoLinkerInfix@_LINKER=@targetPrefix@cc
-    export CARGO_TARGET_@cargoLinkerInfix@_RUSTFLAGS=@targetRustcFlags@
-    # export RUSTFLAGS=@targetRustcFlags@
+    export RUSTFLAGS=@targetRustcFlags@
     # Setup compilers
     export HOST_CC=@nativePrefix@cc
     export HOST_CXX=@nativePrefix@c++
