b82ee647ce9f3f00e0a136d14887399f079b823d
Aleksey Sidorov <sauron1987@gmail.com>
2023-01-28 14:06:06 +0400

    Try to add riscv target

--- pkgs/hooks/rustCrossHook.nix
+++ pkgs/hooks/rustCrossHook.nix
@@ -1,7 +1,15 @@
-{ makeSetupHook, stdenv, lib, runCommand, llvmGccCompat, }:
+{ makeSetupHook, stdenv, lib, runCommand, llvmGccCompat, rust }:
 
 let
-  cargoBuildTarget = stdenv.targetPlatform.config;
+  # FIXME: This workaround was taken from the
+  # https://github.com/oxalica/rust-overlay/blob/be3a8a8b59aaec5cd96d3ea6e4470bd14bdd8b37/rust-overlay.nix#L18
+  toRustTarget = platform:
+    if platform.isWasi then
+      "${platform.parsed.cpu.name}-wasi"
+    else
+      rust.toRustTarget platform;
+
+  cargoBuildTarget = toRustTarget stdenv.targetPlatform;
   cargoLinkerInfix = builtins.replaceStrings [ "-" "." ] [ "_" "_" ] (lib.toUpper cargoBuildTarget);
   # Override cargo target dir in order to make it easier to write 
   # complex build scripts
