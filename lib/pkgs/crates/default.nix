21843b7d7ba9e2e8d803d63c2dab6a20c5dabc04
Aleksey Sidorov <sauron1987@gmail.com>
2023-03-17 13:26:12 +0400

    Resolve comments from the chat gpt bot

--- lib/pkgs/crates/default.nix
+++ lib/pkgs/crates/default.nix
@@ -7,15 +7,17 @@ let
     (name: value: name != "all")
     cargoDeps;
 
-  cargoDeps = rec {
+  protobuf = prev.callPackage ./protobuf.nix { };
+
+  cargoDeps = {
     audiopus_sys = prev.callPackage ./audiopus_sys.nix { };
     rdkafka-sys = prev.callPackage ./rdkafka-sys.nix { };
     rocksdb-sys = prev.callPackage ./rocksdb-sys.nix { };
     zstd-sys = prev.callPackage ./zstd-sys.nix { };
     openssl-sys = prev.callPackage ./openssl-sys.nix { };
     # Popular crates depends on the protoc compiler
-    prost = prev.callPackage ./protobuf.nix { };
-    tonic = prost;
+    prost = protobuf;
+    tonic = protobuf;
     # The special hook to list all cargo packages.
     all = lib.attrValues filteredDeps;
   };
