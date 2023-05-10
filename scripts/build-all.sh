882cc6753ae693b94e4654fa239e14ff29e63a44
Aleksey Sidorov <sauron1987@gmail.com>
2023-05-10 16:26:12 +0400

    Modify build-all script

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -6,7 +6,6 @@ set -euf -o pipefail
 CROSS_SYSTEMS=(
     'null'
     '{ config = "x86_64-unknown-linux-musl"; useLLVM = false; isStatic = false; }'
-    '{ config = "x86_64-unknown-linux-musl"; useLLVM = false; isStatic = true; }'
     '{ config = "x86_64-unknown-linux-musl"; useLLVM = true; isStatic = false; }'
     '{ config = "x86_64-unknown-linux-musl"; useLLVM = true; isStatic = true; }'
 )
@@ -16,9 +15,9 @@ do
     echo "-> Compiling '${CROSS_SYSTEM}' cross system"
     BUILD_OUTPUT=$(nix-build shell.nix -A inputDerivation --arg crossSystem "${CROSS_SYSTEM}")
 
-    echo "-> Performing cross-system testing"
-    nix-shell --arg crossSystem "${CROSS_SYSTEM}" --run ./tests/crates/build_all.sh
+    echo "-> Performing '${CROSS_SYSTEM}' testing"
+    nix-shell --pure --arg crossSystem "${CROSS_SYSTEM}" --run ./tests/crates/build_all.sh
 
-    echo "-> Pushing artifacts to the Cachix"
+    echo "-> Pushing '${CROSS_SYSTEM}' artifacts to the Cachix"
     cachix push nixpkgs-cross-overlay "$BUILD_OUTPUT"
 done
