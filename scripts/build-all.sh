ca934a9a886b649479e75961cbce26f93eb456a4
Aleksey Sidorov <sauron1987@gmail.com>
2023-11-27 23:47:00 +0300

    Update deps and remove obsolete packages

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -5,11 +5,12 @@ set -eufx -o pipefail
 # Supported cross systems
 CROSS_SYSTEMS=(
   'null'
-  '{ config = "x86_64-unknown-linux-musl"; useLLVM = false; isStatic = false; }'
   '{ config = "x86_64-unknown-linux-musl"; useLLVM = true; isStatic = false; }'
   '{ config = "x86_64-unknown-linux-musl"; useLLVM = true; isStatic = true; }'
   '{ config = "aarch64-unknown-linux-musl"; useLLVM = true; isStatic = false; }'
   '{ config = "aarch64-unknown-linux-musl"; useLLVM = true; isStatic = true; }'
+  '{ config = "x86_64-unknown-linux-musl"; useLLVM = false; isStatic = false; }'
+  '{ config = "aarch64-unknown-linux-musl"; useLLVM = false; isStatic = false; }'
 )
 
 for CROSS_SYSTEM in "${CROSS_SYSTEMS[@]}"
