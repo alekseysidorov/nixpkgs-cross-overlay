49d62a884257f66a2f0afdf6ebcc1244527209e7
Aleksey Sidorov <sauron1987@gmail.com>
2024-05-26 13:41:21 +0300

    Disable boost also for musl

--- shell.nix
+++ shell.nix
@@ -30,7 +30,7 @@ pkgs.mkShell {
     bash
     toml11
     nano
-  ] ++ lib.optionals (!stdenv.cc.isClang) [
+  ] ++ lib.optionals (!stdenv.cc.isClang && !stdenv.targetPlatform.isMusl) [
     msgpack-cxx
     boost178
   ];
